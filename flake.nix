53e58c506dad6db23971becbfd32384e7506a474
Fenhl <fenhl@fenhl.net>
2026-08-10 07:17:10 +0000

    Initial library function with devShells

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+    outputs = attrs: {
+        lib = {
+            systems ? [
+                "aarch64-darwin" # intercal
+                "aarch64-linux" # reiwa, thermidor
+                "x86_64-darwin" # bureflux
+                "x86_64-linux" # bureflux, caturday, dushanbe, peterpc3, september
+            ],
+            devShells ? {},
+        }: let
+            merge = builtins.foldl' (a: b: a // b) {};
+            eachSystem = f: merge (builtins.map (system: {
+                ${system} = f system;
+            }) systems);
+        in merge [
+            (if devShells == {} then {} else {
+                devShells = eachSystem (system: let
+                    pkgs = attrs.nixpkgs.legagyPackages.${system};
+                in builtins.mapAttrs (devShell: pkgs.mkShell (devShell {
+                    inherit pkgs;
+                })) devShells);
+            })
+        ];
+    };
+}
